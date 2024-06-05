18060b3a7f53535c02309cd1e6f0fc811860e752
ayonbit <suprovatbit@gmail.com>
2024-06-06 02:32:48 +0600

    Contact Fixed

--- src/scenes/Contact.jsx
+++ src/scenes/Contact.jsx
@@ -18,7 +18,7 @@ const Contact = () => {
   };
 
   return (
-    <section id="contact" className="contact py-48">
+    <section id="contact" className="contact mt-10 py-48">
       {/* HEADINGS */}
       <motion.div
         initial="hidden"
